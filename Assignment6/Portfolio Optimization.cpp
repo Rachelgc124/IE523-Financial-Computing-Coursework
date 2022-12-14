133b207c04cc5d8a51180204f975ccf8eda4b8da
Rachel Gao <63959344+Rachelgc124@users.noreply.github.com>
2022-12-14 09:23:07 -0600

    Add files via upload

---
+++ Assignment6/Portfolio Optimization.cpp
@@ -0,0 +1,16 @@
+//
+//  Portfolio Optimization.cpp
+//  Programming Assignment6: Portfolio Optimization Software in C++ under Parallel Shifts in Term Structure
+//  Created by Chang Gao on 2022/10/20.
+//
+
+#include <iostream>
+#include "portfolio optimization.h"
+
+int main(int argc, const char * argv[]) {
+    
+    portfolio_optimization portfolio;
+    
+    portfolio.optimized(argc, argv);
+    
+}
